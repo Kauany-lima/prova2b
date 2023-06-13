4c89017f4b128af9650a6eb6e2e498ec26b90514
Kauany <Kauany.lima@estudante.ifro.edu.br>
2023-06-13 14:50:10 -0400

    Prova

---
+++ prova2b/correcao.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace prova2b
+{
+    partial class correcao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tx_2 = new System.Windows.Forms.TextBox();
+            this.tx_3 = new System.Windows.Forms.TextBox();
+            this.tx_5 = new System.Windows.Forms.TextBox();
+            this.tx_10 = new System.Windows.Forms.TextBox();
+            this.tx_8 = new System.Windows.Forms.TextBox();
+            this.tx_9 = new System.Windows.Forms.TextBox();
+            this.tx_7 = new System.Windows.Forms.TextBox();
+            this.tx_6 = new System.Windows.Forms.TextBox();
+            this.tx_4 = new System.Windows.Forms.TextBox();
+            this.tx_1 = new System.Windows.Forms.TextBox();
+            this.calcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tx_2
+            // 
+            this.tx_2.Location = new System.Drawing.Point(12, 165);
+            this.tx_2.Name = "tx_2";
+            this.tx_2.Size = new System.Drawing.Size(76, 20);
+            this.tx_2.TabIndex = 0;
+            // 
+            // tx_3
+            // 
+            this.tx_3.Location = new System.Drawing.Point(12, 204);
+            this.tx_3.Name = "tx_3";
+            this.tx_3.Size = new System.Drawing.Size(76, 20);
+            this.tx_3.TabIndex = 1;
+            // 
+            // tx_5
+            // 
+            this.tx_5.Location = new System.Drawing.Point(12, 256);
+            this.tx_5.Name = "tx_5";
+            this.tx_5.Size = new System.Drawing.Size(76, 20);
+            this.tx_5.TabIndex = 2;
+            // 
+            // tx_10
+            // 
+            this.tx_10.Location = new System.Drawing.Point(12, 418);
+            this.tx_10.Name = "tx_10";
+            this.tx_10.Size = new System.Drawing.Size(76, 20);
+            this.tx_10.TabIndex = 3;
+            // 
+            // tx_8
+            // 
+            this.tx_8.Location = new System.Drawing.Point(12, 344);
+            this.tx_8.Name = "tx_8";
+            this.tx_8.Size = new System.Drawing.Size(76, 20);
+            this.tx_8.TabIndex = 4;
+            // 
+            // tx_9
+            // 
+            this.tx_9.Location = new System.Drawing.Point(12, 381);
+            this.tx_9.Name = "tx_9";
+            this.tx_9.Size = new System.Drawing.Size(76, 20);
+            this.tx_9.TabIndex = 5;
+            // 
+            // tx_7
+            // 
+            this.tx_7.Location = new System.Drawing.Point(12, 308);
+            this.tx_7.Name = "tx_7";
+            this.tx_7.Size = new System.Drawing.Size(76, 20);
+            this.tx_7.TabIndex = 6;
+            // 
+            // tx_6
+            // 
+            this.tx_6.Location = new System.Drawing.Point(12, 282);
+            this.tx_6.Name = "tx_6";
+            this.tx_6.Size = new System.Drawing.Size(76, 20);
+            this.tx_6.TabIndex = 7;
+            // 
+            // tx_4
+            // 
+            this.tx_4.Location = new System.Drawing.Point(12, 230);
+            this.tx_4.Name = "tx_4";
+            this.tx_4.Size = new System.Drawing.Size(76, 20);
+            this.tx_4.TabIndex = 8;
+            // 
+            // tx_1
+            // 
+            this.tx_1.Location = new System.Drawing.Point(12, 139);
+            this.tx_1.Name = "tx_1";
+            this.tx_1.Size = new System.Drawing.Size(76, 20);
+            this.tx_1.TabIndex = 9;
+            // 
+            // calcular
+            // 
+            this.calcular.Location = new System.Drawing.Point(150, 344);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(75, 23);
+            this.calcular.TabIndex = 10;
+            this.calcular.Text = "calcular";
+            this.calcular.UseVisualStyleBackColor = true;
+            this.calcular.Click += new System.EventHandler(this.calcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 384);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // correcao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calcular);
+            this.Controls.Add(this.tx_1);
+            this.Controls.Add(this.tx_4);
+            this.Controls.Add(this.tx_6);
+            this.Controls.Add(this.tx_7);
+            this.Controls.Add(this.tx_9);
+            this.Controls.Add(this.tx_8);
+            this.Controls.Add(this.tx_10);
+            this.Controls.Add(this.tx_5);
+            this.Controls.Add(this.tx_3);
+            this.Controls.Add(this.tx_2);
+            this.Name = "correcao";
+            this.Text = "correcao";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tx_2;
+        private System.Windows.Forms.TextBox tx_3;
+        private System.Windows.Forms.TextBox tx_5;
+        private System.Windows.Forms.TextBox tx_10;
+        private System.Windows.Forms.TextBox tx_8;
+        private System.Windows.Forms.TextBox tx_9;
+        private System.Windows.Forms.TextBox tx_7;
+        private System.Windows.Forms.TextBox tx_6;
+        private System.Windows.Forms.TextBox tx_4;
+        private System.Windows.Forms.TextBox tx_1;
+        private System.Windows.Forms.Button calcular;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ prova2b/correcao.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prova2b
+{
+    public partial class correcao : Form
+    {
+        public correcao()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void calcular_Click(object sender, EventArgs e)
+        {
+         
+
+
+
+            // Array array = new Array[10];
+            string[] gab = { "a", "b", "d", "e", "c", "d", "a", "d", "e", "b" };
+
+            //Array gab[10] = { "a", "b", "d",  "e", "c", "d", "a", "d", "e", "b" };
+            int soma = 0;
+
+            if (gab.Length == Convert.ToInt32(tx_1.Text)) 
+            {
+                soma += 1;
+            }
+            else
+            {
+                soma -= 1;
+            }
+
+
+
+        }
+    }
+}
